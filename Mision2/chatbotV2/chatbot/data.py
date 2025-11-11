ba579119b8d40ea407060415f4ccea8a246e6e10
JuanDavidRiosCardona <juandavidrios1531@gmail.com>
2025-11-11 11:47:51 -0500

    IA 2 FUNCIONAL

--- Mision2/chatbotV2/chatbot/data.py
+++
@@ -1,8 +0,0 @@
-training_data =[
-        ("hola","!Hola ¿En que puedo ayudarte"),
-        ("buenos dias ","!Buenos Días¡"),
-        ("cómo estás","Estoy bien, gracias por preguntar"),
-        ("adiós","!Hasta luego¡"),
-        ("tu nombre","Soy un chatbot de ejemplo"),
-        ("que puedes hacer"," Puedo responder preguntas simples basadas en ejemplos ")
-     ]
